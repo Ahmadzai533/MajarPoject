e5751f2d6199619b0ef9cc0a0b2adf19c009b002
Roohullah Ahmadzai <roohullahpoohan@gmail.com>
2026-05-14 18:29:06 +0430

    index route

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const app = express();
 const mongoose = require("mongoose");
 const Mongo_Url = "mongodb://127.0.0.1:27017/wonderlust";
 const Listing = require("./Models/Listing");
+const path = require("path");
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 main()
   .then(() => {
@@ -15,23 +20,27 @@ async function main() {
   await mongoose.connect(Mongo_Url);
 }
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
 app.get("/listing", async (req, res) => {
-  let simpleListing = new Listing({
-    title: "Beautiful Beach House",
-    description:
-      "A stunning beach house with breathtaking ocean viws, perfect for a relaxing getaway.",
-    price: 400,
-    location: "kabul, Afshar",
-    country: "Afghanistan",
-  })
-    .save()
-    .then((listing) => {
-      res.send("Listing created successfully: ");
-    });
+  let allListings = await Listing.find({});
+  res.render("listings/index.ejs", { listings: allListings });
 });
+// app.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
+// app.get("/listing", async (req, res) => {
+//   let simpleListing = new Listing({
+//     title: "Beautiful Beach House",
+//     description:
+//       "A stunning beach house with breathtaking ocean viws, perfect for a relaxing getaway.",
+//     price: 400,
+//     location: "kabul, Afshar",
+//     country: "Afghanistan",
+//   })
+//     .save()
+//     .then((listing) => {
+//       res.send("Listing created successfully: ");
+//     });
+// });
 
 app.listen(8080, () => {
   console.log("Server is running on port 8080");
