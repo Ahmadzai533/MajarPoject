5714b81d64b387f5ca501ebb45b13f4ca90ef867
Roohullah Ahmadzai <roohullahpoohan@gmail.com>
2026-06-16 22:45:08 +0430

    add some new things

---
+++ utils/ExpressError.js
@@ -0,0 +1,8 @@
+class ExpressError extends Error{
+    constructor(status,massege){
+        super()
+        this.status=status;
+        this.massege=massege;
+    }
+}
+module.exports=ExpressError;
